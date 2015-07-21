2fe1da5f90687791708e36b018cf2c554503009a
Tony <tonylefler@gmail.com>
2015-07-21 12:04:29 -0600

    grunt live reload

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt){
+
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            livereload: {
+                files: [
+                    'src/**/*.js',
+                    'src/**/*.html',
+                    'src/sass/main.css'
+                ],
+                tasks: [],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                livereload: 35729,
+                base: ['src']
+            },
+            livereload: {
+                options: {
+                    open: true
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('serve', function (target) {
+
+        grunt.task.run([
+            'connect:livereload',
+            'watch:livereload'
+        ]);
+    });
+
+    grunt.registerTask('default', []);
+};
